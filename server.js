558213a6ee4cbec8ead291b2da09fe09f25c3254
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-03-09 17:15:37 -0600

    bcs help

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    // maxAge: 300000,
-    // httpOnly: true,
-    // secure: false,
-    // sameSite: 'strict',
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
   },
   resave: false,
   saveUninitialized: false,

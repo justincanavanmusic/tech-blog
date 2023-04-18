ee4f7fd003a7098d968b58af6b22f0452b027cdc
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-04-17 22:24:47 -0500

    edit comment button working

--- public/js/edit-comment.js
+++ public/js/edit-comment.js
@@ -15,10 +15,10 @@ async function editComment(event) {
         },
       });
       if (responseObject.ok) {
-        const commentBtn = document.querySelector('comment-btn')
+        const commentBtn = document.querySelector('#comment-btn')
         const postId = commentBtn.getAttribute('data-id');
         console.log(postId);
-        document.location.replace(`api/post/${postId}`);
+        document.location.replace(`/post/${postId}`);
         // document.location.replace(`/dashboard`);
       } else {
         alert('Failed to edit post');

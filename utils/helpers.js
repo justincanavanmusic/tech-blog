ed18e5870be4a2742ac821d7d3aab9b385c4622f
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-04-20 11:46:11 -0500

    new branch

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,14 @@
 module.exports = {
   format_date: date => {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  },
+
+  comparison: (a, b) => {
+    if(a==b) {
+      return true;
+    } else {
+      return false;
+    }
   }
+
 };

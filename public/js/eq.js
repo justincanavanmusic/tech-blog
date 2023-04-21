8ab584f2623652cb7b183b51cdca33e7e915a95c
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-04-21 08:28:50 -0500

    designed dashboard page, starting on homepage

--- public/js/eq.js
+++
@@ -1,6 +0,0 @@
-var Handlebars = require('handlebars');
-
-Handlebars.registerHelper('eq', function( a, b ){
-	var next =  arguments[arguments.length-1];
-	return (a === b) ? next.fn(this) : next.inverse(this);
-});

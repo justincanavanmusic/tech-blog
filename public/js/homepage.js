26976ff3d2744686695c9e3d5aa3df72b324443c
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-04-25 09:25:36 -0500

    finished tech blog title

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,3 +1,12 @@
+let uNameDiv=document.querySelector(".u-name-div")
+let divId = uNameDiv.getAttribute('data-id')
+console.log(divId);
+
+if(window.innerWidth<768) {
+
+}
+
+
 async function viewCommentPage(event) {
 
 let btnId = event.target.getAttribute('data-id')
